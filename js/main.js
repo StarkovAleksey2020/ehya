8444350b976e6b0ef8e0ed6aae00101d00db1ccb
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2020-12-17 21:36:30 +0800

    added: tabs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
   var menuButton = $(".menu-button")
   menuButton.on('click', function () {
-    /*console.log('menuButton: ', menuButton);*/
     $(".navbar-bottom").toggleClass("navbar-bottom--visible");
   })
+
+  var tabsItem = $('.trends-title__tabs-item');
+  var contentItem = $('.trends-cards');
+
+  tabsItem.on("click", function(event) {
+    var activeContent = $(this).attr("data-target");
+    tabsItem.removeClass("trends-title__tabs-item--accent");
+    console.log('activeContent: ',activeContent);
+    contentItem.removeClass("trends-cards--visible");
+    $(activeContent).addClass("trends-cards--visible");
+    $(this).addClass("trends-title__tabs-item--accent");
+  });
 });

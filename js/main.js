1eee171e501c4bcf46b4eefac6f6335cae813014
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2020-12-22 01:35:57 +0800

    added: fixed errors

--- js/main.js
+++ js/main.js
@@ -112,4 +112,39 @@ $(document).ready(function () {
     });
   });
 
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal)
+
+  $(document).keyup(function (e) {
+    if (e.keyCode === 27) {
+      closeModal(e);
+    }
+  });
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+    $('input').val('');
+    $('textarea').val('');
+    $("input").removeClass("invalid");
+    $("label").remove(".invalid");
+    modalDialog.scrollTop(0);
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+    $("input").removeClass("invalid");
+    $("label").remove(".invalid");
+}
+
+// Маска номера телефона
+  $(".phoneInput").mask("+7 (999) 999-99-99");
+
 });
